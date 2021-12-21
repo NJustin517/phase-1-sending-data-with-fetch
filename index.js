aeb5a98502e96f3b274cd6be34da1dfceace27e3
Justin Luciano <justinlluciano@gmail.com>
2021-12-21 13:26:29 -0800

    commit tree 56cdf5dd31f92ff1c0e2d89d19573202eab7ff42

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(username, userEmail) {
+  const login = {
+    name: username,
+    email: userEmail,
+  };
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(login),
+  })
+    .then((resp) => resp.json())
+    .then((json) => {
+      const p = document.createElement("p");
+      p.textContent = json.id;
+      document.querySelector("body").appendChild(p);
+    })
+    .catch((error) => {
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      document.querySelector("body").appendChild(p);
+    });
+}
+
+submitData("NJustin", "P@55W0RD");
